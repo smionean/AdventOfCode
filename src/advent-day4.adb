50c8cb2b35685a8ade64b41b4682b3dc355e06d3
Simon Beàn <smionean@gmail.com>
2018-12-14 16:42:25 -0500

    structure pour les donnees

--- src/advent-day4.adb
+++ src/advent-day4.adb
@@ -8,33 +8,46 @@ with GNAT.Calendar.Time_IO;
 
 package body Advent.Day4 is
 
+type Evenement_Type is (sleep,awake,change_shift);
+
+type Evenement_Record is
+record
+Evenement : Evenement_Type;
+Elf_Id : Natural;
+end record;
 
 
-     package dictionnaire_horaire is new Ada.Containers.Ordered_Maps(Key_Type     => Ada.Calendar.Day_Duration,
-                                                                     Element_Type => Natural);
+package dictionnaire_horaire is new Ada.Containers.Ordered_Maps(Key_Type => Ada.Calendar.Day_Duration,  -- Heure en secondes
+  Element_Type => Evenement_Record);
+
+     package dictionnaire_jour is new Ada.Containers.Ordered_Maps(Key_Type     => Ada.Calendar.Day_Duration, -- Date en secondes
+                                                                     Element_Type => dictionnaire_horaire);
 
 
 
    ------------------------------
    --
    ------------------------------
-   procedure Decortiquer_Info (La_Ligne : in String) is
+   procedure Decortiquer_Info (La_Ligne : in String, Les_Info : in out dictionnaire_jour.Map) is
       Position_Espace : Natural := 1;
       Position_Crochet: Natural := 1;
    begin
       Position_Espace := Index (La_Ligne, " ", 1);
       Position_Crochet := Index (La_Ligne, "]", 1);
       declare
-         une_Date_Tmp : String := La_Ligne(La_Ligne'First + 1..Position_Crochet + 2);
+         une_Date_Tmp : String := La_Ligne(La_Ligne'First + 1..Position_Espace-1);
+         une_Heure_Tmp : String := La_Ligne(Position_Espace+1..Position_Crochet-1);
          un_Evenement :  String:= La_Ligne(Position_Crochet + 2..La_Ligne'Last);
          une_Date : Ada.Calendar.Day_Duration;
+         une_Heure : Ada.Calendar.Day_Duration;
       begin
          une_Date_Tmp(une_Date_Tmp'First..une_Date_Tmp'First+1):="20"; -- changer annee 1518 pour 2018
-         une_Date_Tmp(une_Date_Tmp'Last-2..une_Date_Tmp'Last):=":00"; -- ajouter les secondes
+         --une_Date_Tmp(une_Date_Tmp'Last-2..une_Date_Tmp'Last):=":00"; -- ajouter les secondes
 
          Put_Line(une_Date_Tmp&" "&un_Evenement);
 
          une_Date := Ada.Calendar.Seconds(Date => GNAT.Calendar.Time_IO.Value(Date => une_Date_Tmp) );
+         une_Heure := Ada.Calendar.Seconds(Date => GNAT.Calendar.Time_IO.Value(Date => une_Heure_Tmp) );
 
       end;
 
