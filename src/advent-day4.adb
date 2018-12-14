a6db2137f9052c11533f54d041bb1dd0d3eb7c71
Simon Bean <smionean@gmail.com>
2018-12-13 22:43:13 -0500

    Essai

--- src/advent-day4.adb
+++ src/advent-day4.adb
@@ -1,12 +1,17 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Text_IO.Text_Streams;  use Ada.Text_IO.Text_Streams;
 with Ada.Containers.Vectors;
+with Ada.Containers.Ordered_Maps;
 with Ada.Strings.Fixed;  use Ada.Strings.Fixed;
 with Ada.Calendar;
+with GNAT.Calendar.Time_IO;
 
 package body Advent.Day4 is
 
-   type Tissu_Type is array (Integer range <>, Integer range <>) of Integer;
+
+
+     package dictionnaire_horaire is new Ada.Containers.Ordered_Maps(Key_Type     => Ada.Calendar.Day_Duration,
+                                                                     Element_Type => Natural);
 
 
 
@@ -14,8 +19,26 @@ package body Advent.Day4 is
    --
    ------------------------------
    procedure Decortiquer_Info (La_Ligne : in String) is
+      Position_Espace : Natural := 1;
+      Position_Crochet: Natural := 1;
    begin
-      null;
+      Position_Espace := Index (La_Ligne, " ", 1);
+      Position_Crochet := Index (La_Ligne, "]", 1);
+      declare
+         une_Date_Tmp : String := La_Ligne(La_Ligne'First + 1..Position_Crochet + 2);
+         un_Evenement :  String:= La_Ligne(Position_Crochet + 2..La_Ligne'Last);
+         une_Date : Ada.Calendar.Day_Duration;
+      begin
+         une_Date_Tmp(une_Date_Tmp'First..une_Date_Tmp'First+1):="20"; -- changer annee 1518 pour 2018
+         une_Date_Tmp(une_Date_Tmp'Last-2..une_Date_Tmp'Last):=":00"; -- ajouter les secondes
+
+         Put_Line(une_Date_Tmp&" "&un_Evenement);
+
+         une_Date := Ada.Calendar.Seconds(Date => GNAT.Calendar.Time_IO.Value(Date => une_Date_Tmp) );
+
+      end;
+
+
    end Decortiquer_Info;
 
    ------------------------------
