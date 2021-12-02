c6d3320af54740da87c4a868e503ff659a6421bd
Simon Beàn <smionean@gmail.com>
2021-12-02 12:23:01 -0500

    correction pour script de generation

--- 2021/template/generate.sh
+++ 2021/template/generate.sh
@@ -7,10 +7,9 @@ sed -f "${FICHIER_SED}" < "${DIR}/dayX.adb" > "${DIR}/day${1}.utf8.adb"
 sed -f "${FICHIER_SED}" < "${DIR}/dayX.gpr" > "${DIR}/day${1}.utf8.gpr"
 rm -f "${FICHIER_SED}"
 
-mkdir -p "../day${1}/src"
-mkdir -p "../day${1}/data"
+mkdir -p "${DIR}/../day${1}/src"
+mkdir -p "${DIR}/../day${1}/data"
 iconv -f UTF-8 -t ISO-8859-1 "${DIR}/day${1}.utf8.adb" > "${DIR}/../day${1}/src/day${1}.adb"
 iconv -f UTF-8 -t ISO-8859-1 "${DIR}/day${1}.utf8.gpr" > "${DIR}/../day${1}/day${1}.gpr"
 
 rm -f "${DIR}/day${1}.utf8.gpr" "${DIR}/day${1}.utf8.adb"
-
