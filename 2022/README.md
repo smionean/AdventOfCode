0bf3d252680b24331d62e264d79d1c4f286c4bf0
Simon Beàn <smionean@gmail.com>
2022-11-25 23:18:51 -0500

    Préparation pour le challenge "Advent of Code 2022"

---
+++ 2022/README.md
@@ -0,0 +1,20 @@
+# Défi 2022 en Ada 
+(https://adventofcode.com/2022)
+
+Pour créer le projet de la journée (exemple : jour 01)
+```
+template/generate.sh 02
+```
+
+Pour compiler
+```
+gprbuild day01.gpr
+```
+
+Pour exécuter
+```
+obj/day01
+```
+
+Pour soumettre : 
+https://forum.ada-lang.io/t/advent-of-ada-submissions/129
---
+++ 2022/template/dayX.adb
@@ -0,0 +1,74 @@
+--+-------------------------------------------------------------------------+-- 
+--|                                                                         |--
+--|                                                                         |--
+--|                                  TTT                                    |--
+--|                                  TTT                                    |--
+--|                                  TTT                                    |--
+--|                           TTT           TTT                             |--
+--|                           TTT           TTT                             |--
+--|                           TTTTTTTTTTTTTTTTT                             |--
+--|                           TTTTTTTTTTTTTTTTT                             |--
+--|                           TTT           TTT                             |--
+--|                           TTT           TTT                             |--
+--|                                                                         |--
+--|                          github.com/smionean                            |--
+--|                                                                         |--
+--| 																																		    |--
+--+-------------------------------------------------------------------------+--
+--|   * Project name : _PROJECTNAME
+--|   * Name : Simon Beàn
+--|   * Date : _DATE
+--|   
+--|   * Filename : _FILENAME
+--|
+--|   * Description (fr) : Défi Calnedrier de l'Avent 2022
+--|                        Jour _DEFIX
+--|
+--|                 (en) : Advent Of Code Challenge 2022
+--|                        Day _DEFIX
+--|
+--|   #AdaAdventOfCode22
+--|
+--+-------------------------------------------------------------------------+--
+--|   * Changelog * 
+--|
+--|
+--+-------------------------------------------------------------------------+--
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO.Text_Streams;  use Ada.Text_IO.Text_Streams;
+with Ada.Containers.Vectors;
+
+procedure Day_DEFIX is
+
+   procedure Execute(fichier : in String) is
+      Input : File_Type;
+      Reponse : Integer := 0;
+      Reponse_2 : Integer := 0;
+   begin
+      Open (File => Input,
+         Mode => In_File,
+            Name => fichier);
+      -- Get Data
+      While not  End_Of_File (Input) Loop
+         declare
+            Line : String := Get_Line (Input);
+         begin
+            null;
+         end;
+      end loop;
+      Close(Input);
+
+            
+      Put_Line("Results (part1) : " & Reponse'Img);
+      Put_Line("Results (part2) : " & Reponse_2'Img);
+   end Execute;
+
+begin
+   Put_Line("TEST");
+   Execute("data/test.txt");
+   
+   New_Line;
+   Put_Line("CHALLENGE DAY _DEFIX");
+   Execute("data/data.txt");
+end Day_DEFIX;
