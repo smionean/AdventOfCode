6d98a701e7590f812f148986adc426f14a465f9b
sbean <smionean@gmail.com>
2022-12-02 08:26:18 -0500

    preparation jour 2

---
+++ 2022/day02/src/day02.adb
@@ -0,0 +1,74 @@
+--+-------------------------------------------------------------------------+-- 
+--|                                                                         |--
+--|                                                                         |--
+--|                                  TTT                                    |--
+--|                                  TTT                                    |--
+--|                                  TTT                                    |--
+--|                           TTT           TTT                             |--
+--|                           TTT           TTT                             |--
+--|                           TTTTTTTTTTTTTTTTT                             |--
+--|                           TTTTTTTTTTTTTTTTT                             |--
+--|                           TTT           TTT                             |--
+--|                           TTT           TTT                             |--
+--|                                                                         |--
+--|                          github.com/smionean                            |--
+--|                                                                         |--
+--|                                                                         |--
+--+-------------------------------------------------------------------------+--
+--|   * Project name : Advent of Code 2022 : 02
+--|   * Name : Simon Beàn
+--|   * Date : Fri 02 Dec 2022 08:24:29 AM EST
+--|   
+--|   * Filename : day02.adb
+--|
+--|   * Description (fr) : Défi Calnedrier de l'Avent 2022
+--|                        Jour 02
+--|
+--|                 (en) : Advent Of Code Challenge 2022
+--|                        Day 02
+--|
+--|   #AdaAdventOfCode22
+--|
+--+-------------------------------------------------------------------------+--
+--|   * Changelog * 
+--|
+--|
+--+-------------------------------------------------------------------------+--
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO.Text_Streams;  use Ada.Text_IO.Text_Streams;
+with Ada.Containers.Vectors;
+
+procedure Day02 is
+
+   procedure Execute(fichier : in String) is
+      Input : File_Type;
+      Reponse : Integer := 0;
+      Reponse_2 : Integer := 0;
+   begin
+      Open (File => Input,
+         Mode => In_File,
+            Name => fichier);
+      -- Get Data
+      While not  End_Of_File (Input) Loop
+         declare
+            Line : String := Get_Line (Input);
+         begin
+            null;
+         end;
+      end loop;
+      Close(Input);
+
+            
+      Put_Line("Results (part1) : " & Reponse'Img);
+      Put_Line("Results (part2) : " & Reponse_2'Img);
+   end Execute;
+
+begin
+   Put_Line("TEST");
+   Execute("data/test.txt");
+   
+   New_Line;
+   Put_Line("CHALLENGE DAY 02");
+   Execute("data/data.txt");
+end Day02;
